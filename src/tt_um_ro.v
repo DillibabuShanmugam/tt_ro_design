28ceb45871839348ebcb66f9d8ccd891b3be1734
DillibabuShanmugam <90065623+DillibabuShanmugam@users.noreply.github.com>
2024-08-29 10:07:00 -0400

    Update tt_um_ro.v

--- src/tt_um_ro.v
+++ src/tt_um_ro.v
@@ -17,11 +17,30 @@ module tt_um_ro (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  //assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
+
+// Instantiate the ro_buffer_counter module
+  wire [7:0] buffer_out;
+
+  ro_buffer ro_buffer_inst (.rst_n(rst_n),
+                            .clk(clk),
+                            .ro_activate_1(ui_in[0]),
+                            .ro_activate_2(ui_in[1]),
+			                .out_sel(ui_in[4:2]),
+                            .out(buffer_out)
+                           );
+
+  // Example: Output assignments (update based on your design needs)
+  assign uo_out  = buffer_out;  // Example: take lower 8 bits of buffer_out
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, ui_in[7:5],  1'b0};
 
 endmodule
