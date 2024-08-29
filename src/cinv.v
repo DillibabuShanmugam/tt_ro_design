1d4ba056549f3373ab86323bec6a9b16233aa04f
DillibabuShanmugam <90065623+DillibabuShanmugam@users.noreply.github.com>
2024-08-29 11:33:45 -0400

    Update cinv.v

--- src/cinv.v
+++ src/cinv.v
@@ -1,14 +1,11 @@
 (* keep_hierarchy = "yes" *)
-module cinv#(	
-  parameter real INV_DELAY_NS = 0.07 /* single inverter delay */
-) (input a,            output q);
+module cinv (input a,            output q);
 
-not #(INV_DELAY_NS) (y, a);
 
-// `ifdef SIM
-//    assign  q =  ~a;
-// `else
-//    assign q = ~a;
-// `endif
+`ifdef SIM
+   assign #2 q =  ~a;
+`else
+   assign q = ~a;
+`endif
 
 endmodule
